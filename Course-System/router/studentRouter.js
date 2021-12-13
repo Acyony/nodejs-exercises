2fdcc845e86e66549844dbd25502041cc9da34d0
Alcione <alcionefribeiro@gmail.com>
2021-12-13 22:07:00 +0100

    Add express-validation to add user

--- Course-System/router/studentRouter.js
+++ Course-System/router/studentRouter.js
@@ -2,7 +2,14 @@ const router = require("express").Router();
 const {body, validationResult} = require('express-validator');
 const { addStudent, classParticipants, allStudent } = require('../controller/studentCrontroller');
 
-router.post("/", addStudent);
+router.post("/",[
+    body("fullName").notEmpty().withMessage('FullName name is required!').trim(),
+    body('email', 'Email is required!').notEmpty().isEmail().normalizeEmail(),
+    body('password', 'Password is required').isLength({min: 4}),
+    body("phone").notEmpty().withMessage('Phone name is required!'),
+    body("address").notEmpty().withMessage('Address name is required!'),
+    body("job").notEmpty().withMessage('Job name is required!'),
+], addStudent);
 router.get("/all", allStudent);
 router.get('/:cid', classParticipants);
 
