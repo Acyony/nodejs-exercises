6979bb8c2e3b8d17181bab8de48608f91b535662
Alcione <alcionefribeiro@gmail.com>
2022-01-04 19:04:20 +0100

    add login partial

--- backend-project-exercise/server.js
+++ backend-project-exercise/server.js
@@ -18,7 +18,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render("index")
+    res.render("index", {current: "active"})
+})
+
+app.get('/login', (req, res) => {
+    res.render("login", {current: "active"})
 })
 
 //db connection
